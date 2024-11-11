a7c7a4108166409ef8ef144284fde4169e8cdcdf
Maksim Metelkin <maxwjiill@gmail.com>
2024-11-11 21:44:02 +0300

    added new sloved task

---
+++ 2.1/task_n.py
@@ -0,0 +1,9 @@
+"""module task_n"""
+
+red_count = int(input())
+green_count = int(input())
+blue_count = int(input())
+
+sum_count = red_count + green_count + blue_count
+
+print(sum_count - green_count + 1)
